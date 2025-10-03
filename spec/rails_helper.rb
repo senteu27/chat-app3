130745799b6358ec22be448a4608f8548121b63c
senteu27 <hosomiakashi1227@icloud.com>
2025-10-03 11:31:53 +0900

    結合テストコードの実装

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,7 +25,7 @@ require 'faker'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
+Rails.root.glob('spec/support/**/*.rb').sort_by(&:to_s).each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -36,12 +36,14 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 I18n.locale = "en"
 RSpec.configure do |config|
+  config.include Devise::Test::IntegrationHelpers, type: :system
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
   ]
 
   config.include FactoryBot::Syntax::Methods
+  config.include SignInSupport
 
 
 
